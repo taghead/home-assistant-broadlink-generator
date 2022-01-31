7674637639d26a16885a0498a924f55149c99d80
Andrew Chander <13403032+taghead@users.noreply.github.com>
2022-01-31 17:40:06 +1100

    Added entity_id to script #1

--- index.ts
+++ index.ts
@@ -13,30 +13,41 @@ async function main() {
   const storageDir = `${dir}/.storage/`;
   const scriptFile = `${dir}/scripts.yaml`;
   const devices: any = [];
+  const devicesRegistry = JSON.parse(
+    fs.readFileSync(`${storageDir}/core.entity_registry`, "utf-8")
+  );
 
+  // Push learned codes into devices array.
   const files = await fs.readdirSync(storageDir);
-
   for (const i in files) {
     const file = files[i];
     if (file.startsWith("broadlink_remote") && file.endsWith("codes")) {
       let data = JSON.parse(fs.readFileSync(`${storageDir}${file}`, "utf-8"));
+      // Hook on entity_id onto device
+      for (const entity of devicesRegistry.data.entities) {
+        if (file.includes("_" + entity.unique_id + "_")) {
+          data.entity_id = entity.entity_id;
+        }
+      }
       log(`FOUND ${file}`);
       devices.push(data);
     }
   }
 
+  // Format and write codes into script.yaml
   let fileWrite;
-
   for (const device in devices) {
+    log("\n" + devices[device].entity_id);
+    const entityId = devices[device].entity_id;
     const data: any = devices[device].data;
     for (const deviceId in data) {
-      log(`${deviceId}:`);
+      log(`  ${deviceId}:`);
       for (const command in data[deviceId]) {
         fileWrite == undefined
-          ? (fileWrite = toYAMLScript(deviceId, command))
-          : (fileWrite += toYAMLScript(deviceId, command));
+          ? (fileWrite = toYAMLScript(deviceId, command, entityId))
+          : (fileWrite += toYAMLScript(deviceId, command, entityId));
 
-        log(`  - ${command}`);
+        log(`    - ${command}`);
       }
     }
   }
@@ -46,7 +57,7 @@ async function main() {
   });
 }
 
-function toYAMLScript(deviceId: String, command: String) {
+function toYAMLScript(deviceId: String, command: String, entityId: String) {
   const id = `${simpleSanitize(deviceId)}_${simpleSanitize(command)}`;
   const alias = `${deviceId} ${command}`;
 
@@ -55,7 +66,7 @@ function toYAMLScript(deviceId: String, command: String) {
   sequence:
   - service: remote.send_command
     target:
-      entity_id: remote.broadlink_remote
+      entity_id: ${entityId}
     data:
       device: ${deviceId}
       command: ${command}
